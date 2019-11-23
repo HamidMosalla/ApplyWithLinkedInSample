27fbaeedc99b4769cdcd67aa399b32d97999ed21
Hamid Mosalla <Xellarix@gmail.com>
2019-11-23 16:28:05 +0100

    Fist commit

---
+++ ApplyWithLinkedInSample/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using LinkedInSample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkedInSample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerConfiguration()
+        {
+            return View();
+        }
+
+        public IActionResult ApplyWithLinkedInButton()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
